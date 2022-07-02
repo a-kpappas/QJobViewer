c645faa948c7fa84c042ac8c007325d0be27715e
Anton Pappas <apappas@suse.com>
2022-07-02 20:07:36 +0200

    Start work on JobModel

--- jobmodel.cpp
+++ jobmodel.cpp
@@ -5,3 +5,55 @@ JobModel::JobModel(QObject *parent)
 {
 
 }
+
+
+int SettingsTabModel::rowCount(const QModelIndex &parent) const
+{
+    return parent.isValid() ? 0 : m_list.size();
+}
+
+int SettingsTabModel::columnCount(const QModelIndex &parent) const
+{
+    return parent.isValid() ? 0 : 2;
+}
+
+QVariant SettingsTabModel::data(const QModelIndex &index, int role) const
+{
+    if (!index.isValid())
+        return QVariant();
+
+    if (index.row() >= m_list.size() || index.row() < 0)
+        return QVariant();
+
+    if (role == Qt::DisplayRole) {
+        auto row = m_list.at(index.row());
+
+        switch (index.column()) {
+            case 0:
+                return row.first;
+            case 1:
+                return row.second;
+            default:
+                break;
+        }
+    }
+    return QVariant();
+}
+
+QVariant SettingsTabModel::headerData(int section, Qt::Orientation orientation, int role) const
+{  
+    if (role != Qt::DisplayRole)
+        return QVariant();
+
+    if (orientation == Qt::Horizontal) {
+        switch (section) {
+            case 0:
+                return tr("Variable");
+            case 1:
+                return tr("Value");
+            default:
+                break;
+        }
+    }
+    return QVariant();
+}
--- jobmodel.h
+++ jobmodel.h
@@ -3,14 +3,32 @@
 
 #include <QObject>
 #include <QFile>
+#include <QAbstractTableModel>
+
 class JobModel : public QObject
 {
     Q_OBJECT
+
 public:
     explicit JobModel(QObject *parent = nullptr);
 
 signals:
 
+};
+
+class SettingsTabModel: public QAbstractTableModel{
+    Q_OBJECT
+
+public:
+    explicit SettingsTabModel(QObject *parent = nullptr);
+    SettingsTabModel(const QList<std::pair<QString,QString>>, QObject *parent = nullptr);
+
+    int rowCount(const QModelIndex &parent) const override;
+    int columnCount(const QModelIndex &parent) const override;
+    QVariant data(const QModelIndex &index, int role) const override;
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
+private:
+    QList<QPair<QString,QString>> m_list;
 };
 
 #endif // JOBMODEL_H
